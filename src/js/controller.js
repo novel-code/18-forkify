8265ccf51d953d13c6237e444a1dfee065976b30
syed <novelcode0@gmail.com>
2021-07-30 17:40:29 +0530

    pagination of recipieces

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,9 +6,9 @@ import resultsView from './views/resultsView.js';
 import 'core-js/stable'; // polyfilling everything else
 import 'regenerator-runtime/runtime'; // polyfilling async await
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -37,7 +37,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
